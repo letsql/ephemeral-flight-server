ab8f10614fe216073f614a889f7390daff1c7b3f
Daniel Mesejo <mesejoleon@gmail.com>
2025-01-24 14:34:48 +0100

    chore: assert port is in use

--- demo/tests/test_server.py
+++ demo/tests/test_server.py
@@ -1,3 +1,5 @@
+import socket
+
 import letsql as ls
 import pandas as pd
 import pytest
@@ -6,6 +8,13 @@ import pyarrow as pa
 from demo import EphemeralServer, BasicAuth, make_con
 from util import certificate_path, key_path, scheme, host
 
+def port_in_use(port, host='localhost'):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        try:
+            s.bind((host, port))
+            return False
+        except socket.error:
+            return True
 
 @pytest.mark.parametrize(
     "connection,port",
@@ -15,7 +24,9 @@ from util import certificate_path, key_path, scheme, host
     ],
 )
 def test_create_and_list_tables(connection, port):
-    # check that the port is occupied
+
+    assert not port_in_use(port), f"Port {port} already in use"
+
     with EphemeralServer(
         location="{}://{}:{}".format(scheme, host, port),
         certificate_path=certificate_path,
@@ -32,5 +43,6 @@ def test_create_and_list_tables(connection, port):
         t = con.register(data, table_name="users")
         actual = ls.execute(t)
 
+        assert port_in_use(port)
         assert "users" in con.tables
         assert isinstance(actual, pd.DataFrame)
